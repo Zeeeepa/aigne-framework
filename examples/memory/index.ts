556c6607b9bd2b34befde73b22bb31ef568585bb
Yechao Li <liyechao@yechao.xyz>
2025-04-20 23:25:41 +0800

    feat(example): add memory example

---
+++ examples/memory/index.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env npx -y bun
+
+import { join } from "node:path";
+import { runChatLoopInTerminal } from "@aigne/cli/utils/run-chat-loop.js";
+import { AIAgent, ExecutionEngine } from "@aigne/core";
+import { loadModel } from "@aigne/core/loader/index.js";
+import { AINotebook } from "@aigne/core/memory/ai-notebook.js";
+
+const model = await loadModel();
+
+const engine = new ExecutionEngine({
+  model,
+  tools: [],
+});
+
+const agent = AIAgent.from({
+  name: "memory_example",
+  instructions: `\
+You are a friendly chatbot.
+`,
+  memory: [new AINotebook({ rootDir: join(import.meta.dirname, "memories") })],
+});
+
+const userAgent = engine.call(agent);
+
+await runChatLoopInTerminal(userAgent, {
+  welcome: "Hello! I'm a chatbot with memory. Try asking me a question!",
+});
+
+process.exit(0);
