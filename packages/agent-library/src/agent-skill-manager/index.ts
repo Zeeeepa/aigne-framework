ca01a056ff73ebed7094e1e07964bc06cd3d26d0
Yechao LI <liyechao@yechao.xyz>
2025-12-26 11:18:06 +0800

    fix(agent-library): set default instructions for agent skill manager (#861)

--- packages/agent-library/src/agent-skill-manager/index.ts
+++ packages/agent-library/src/agent-skill-manager/index.ts
@@ -1,4 +1,11 @@
-import { AIAgent, type AIAgentOptions, type Message } from "@aigne/core";
+import {
+  type Agent,
+  AIAgent,
+  type AIAgentOptions,
+  type AIAgentToolChoice,
+  type Message,
+} from "@aigne/core";
+import { type Instructions, instructionsToPromptBuilder } from "@aigne/core/loader/schema.js";
 import { AgentSkillManagerSystemPrompt } from "./prompt.js";
 
 export interface AgentSkillManagerOptions<I extends Message = Message, O extends Message = Message>
@@ -8,10 +15,36 @@ export default class AgentSkillManagerAgent<
   I extends Message = Message,
   O extends Message = Message,
 > extends AIAgent<I, O> {
+  static override async load<I extends Message = any, O extends Message = any>(options: {
+    filepath: string;
+    parsed: object;
+  }): Promise<Agent<I, O>> {
+    interface AIAgentLoadSchema {
+      instructions?: Instructions;
+      autoReorderSystemMessages?: boolean;
+      autoMergeSystemMessages?: boolean;
+      inputKey?: string;
+      inputFileKey?: string;
+      outputKey?: string;
+      outputFileKey?: string;
+      toolChoice?: AIAgentToolChoice;
+      toolCallsConcurrency?: number;
+      keepTextInToolUses?: boolean;
+    }
+
+    const schema = AIAgent.schema<AIAgentLoadSchema>(options);
+    const valid = await schema.parseAsync(options.parsed);
+    return new AgentSkillManagerAgent<I, O>({
+      ...options.parsed,
+      ...valid,
+      instructions: valid.instructions && instructionsToPromptBuilder(valid.instructions),
+    });
+  }
+
   constructor(options: AgentSkillManagerOptions<I, O>) {
     super({
-      instructions: AgentSkillManagerSystemPrompt,
       ...options,
+      instructions: options.instructions || AgentSkillManagerSystemPrompt,
     });
   }
 }
