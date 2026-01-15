e56c0cfbd742413af938b184ecb46f7580fa1f61
Yechao LI <liyechao@yechao.xyz>
2026-01-15 08:55:21 +0800

    test: run AFSHistory in browser (#907)

--- tests/browser/src/main.tsx
+++ tests/browser/src/main.tsx
@@ -1,11 +1,11 @@
+import { AFSHistory } from "@aigne/afs-history";
 import * as core from "@aigne/core";
-import { DefaultMemory } from "@aigne/default-memory";
 import { AIGNEHTTPClient } from "@aigne/transport/http-client/index.js";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-Object.assign(globalThis, { ...core, AIGNEHTTPClient, DefaultMemory });
+Object.assign(globalThis, { ...core, AIGNEHTTPClient, AFSHistory });
 
 createRoot(document.getElementById("root") as Element).render(
   <StrictMode>
